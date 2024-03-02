be68824efdd6cb2f6b617649689605a54e9cdf38
dnavarro <davidnavarrom3@gmail.com>
2024-03-02 09:03:08 +0100

    MINOR- Add Toml Bundles

--- data/datasources/build.gradle.kts
+++ data/datasources/build.gradle.kts
@@ -9,8 +9,7 @@ java {
 }
 
 dependencies{
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("androidx.room:room-runtime:2.3.0")
+    implementation(libs.bundles.data.datasources)
     implementation(project(":data:models"))
     implementation(project(":data:mappers"))
 }
